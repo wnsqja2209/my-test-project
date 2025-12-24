51d5c29a664249afeac5a84b86e15c2841a1b51b
wnsqja2209 <wnsqja2209@gmail.com>
2025-12-24 17:54:05 +0900

    Fix: next-themes 타입 오류 수정 및 빌드 오류 해결

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // lockfile 경고 해결
+  outputFileTracingRoot: require("path").join(__dirname),
 };
 
 export default nextConfig;
